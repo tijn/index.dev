f73b374581b606d192a8e7d9c479bdde46b93753
Tijn Schuurmans <tijn@tijnschuurmans.nl>
2016-03-11 23:29:19 +0100

    find paths using environment vars
    This is how Prax (Crystal) and Pow find their directories. It should
    work for any configuration now.

--- overview.rb
+++ overview.rb
@@ -10,11 +10,22 @@ require 'tilt/sass'
 
 require_relative 'app'
 
-GLOB = "#{ENV['HOME']}/{.prax,.pow}/*"
+POW_ROOT = ENV.fetch('POW_HOST_ROOT', "#{ENV['HOME']}/.pow")
+PRAX_ROOT = ENV.fetch('PRAX_HOSTS', "#{ENV['HOME']}/.prax")
+POW_AND_PRAX = [POW_ROOT, PRAX_ROOT]
+GLOB = "{#{POW_AND_PRAX.join(',')}}/*"
+ICON_PATHS = POW_AND_PRAX.product(['_icons', '.icons']).map { |paths| paths.join('/') }
 
 get '/' do
   @apps = fetch_apps
   slim :'index.html'
+end
+
+get '/icons/:name.png' do |name|
+  full_path = detect_icon(name)
+  raise Sinatra::NotFound if full_path.nil?
+  last_modified File.mtime(full_path)
+  send_file full_path
 end
 
 get '/the_index.css' do
@@ -26,4 +37,8 @@ def fetch_apps
     .select { |name| File.symlink?(name) }
     .map { |name| App.new(name.split('/').last) }
     .sort
+end
+
+def detect_icon(icon)
+  ICON_PATHS.lazy.map { |path| "#{path}/#{icon}.png" }.detect { |file| File.exist? file }
 end
