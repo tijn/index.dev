809148429e8753e4b18cef170dc8af2a26742ee8
Tijn Schuurmans <tijn@tijnschuurmans.nl>
2018-04-07 23:35:18 +0200

    Improve styling

--- public/keyhandler.js
+++ public/keyhandler.js
@@ -1,8 +1,15 @@
 'use strict'
 
-let filter = document.getElementById("filter")
+let filter = document.getElementById("filter-input")
+let container = document.getElementById("filter");
 
 function updateFilter() {
+  if (filter.value === '') {
+    container.classList.remove('filter--active')
+  } else {
+    container.classList.add('filter--active')
+  }
+
   let apps = document.getElementsByClassName("app")
   for (var i=0; i<apps.length; i++) {
     let app = apps[i]
@@ -14,8 +21,11 @@ function updateFilter() {
   }
 }
 
+let navKeys = ['Tab', 'Shift', 'Enter', 'ArrowUp', 'ArrowDown']
+
 document.addEventListener('keydown', (event) => {
-  if (event.key === 'Tab' || event.key === 'Shift') {
+  console.log(event.key)
+  if (navKeys.includes(event.key)) {
     return
   } else if (event.key === 'Escape') {
     filter.value = ''
