da662b15f93345e0062d26fe29471311f890b878
Tijn Schuurmans <tijn@tijnschuurmans.nl>
2018-12-08 14:15:18 +0100

    Search for icons in the approprate xdg dir too

--- config/setup.rb
+++ config/setup.rb
@@ -1,10 +1,20 @@
 require 'dotenv'
 
-POW_ROOT = ENV.fetch('POW_HOST_ROOT', "#{ENV['HOME']}/.pow")
-PRAX_ROOT = ENV.fetch('PRAX_HOSTS', "#{ENV['HOME']}/.prax")
-POW_AND_PRAX = [POW_ROOT, PRAX_ROOT].freeze
-GLOB = "{#{POW_AND_PRAX.join(',')}}/*".freeze
-ICON_PATHS = POW_AND_PRAX.product(['_icons', '.icons']).map { |paths| paths.join('/') }
+# CORDBOARD_ROOT = `cordboard settings | iniq "[directories]" apps`
+def default_dirs
+  pow_root = ENV.fetch('POW_HOST_ROOT', "#{ENV['HOME']}/.pow")
+  prax_root = ENV.fetch('PRAX_HOSTS', "#{ENV['HOME']}/.prax")
+  [pow_root, prax_root]
+end
+
+XDG_DATA_DIR = ENV.fetch('XDG_DATA_DIR', "#{ENV['HOME']}/.local/share")
+DATA_DIR = "#{XDG_DATA_DIR}/index.dev"
+
+APPS_DIR = Array(ENV.fetch('APPS_DIR', default_dirs))
+GLOB = "{#{APPS_DIR.join(',')}}/*".freeze
+
+ICON_PATHS = ["#{DATA_DIR}/icons"] + APPS_DIR.product(['_icons', '.icons']).map { |paths| paths.join('/') }
 ICON_TYPES = %w(svg png).freeze
+
 TLD = ENV.fetch('TLD', 'test')
 HIDE_SELF = ENV.fetch('HIDE_SELF', false) == 'true'
